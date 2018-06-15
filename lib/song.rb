cc66ac2bc6765bf9fd7c41eea0f70c170b74456d
Janice Darikho <janicedarikho@gmail.com>
2018-06-15 10:32:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,6 +33,14 @@ class Song
     }
   end
 
+  def self.find_by_name(name)
+    all.detect{ |s| s.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
   def save
     self.class.all << self
   end
