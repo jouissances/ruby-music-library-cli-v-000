e9ec992cf5ac5f742d4218572114808e9c7e9377
Janice Darikho <janicedarikho@gmail.com>
2018-06-15 10:41:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ class Song
     artist_name = parts[0]
     song_name = parts[1]
     genre_name = parts[2].gsub(".mp3", "")
-    
+
     artist = Artist.find_or_create_by_name(artist_name)
     genre = Genre.find_or_create_by_name(genre_name)
 
